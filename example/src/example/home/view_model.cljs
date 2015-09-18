49f23397c23a37a94fff493b752336d455c65475
Antony Woods <acron1@gmail.com>
2015-09-18 13:05:55 +0100

    Tidying up view-model example code

--- example/src/example/home/view_model.cljs
+++ example/src/example/home/view_model.cljs
@@ -1,48 +1,49 @@
 (ns ^:figwheel-always example.home.view-model
     (:require [om.core :as om :include-macros true]
               [schema.core :as s :include-macros true]
-              [venue.core :as venue])
+              [venue.core :as venue :include-macros true])
     (:require-macros [cljs-log.core :as log]))
 
 (defmulti event-handler
-  (fn [event this args cursor] event))
+  (fn [event owner args cursor] event))
 
 (defmulti response-handler
-  (fn [result data] result))
+    (fn [result owner data] result))
+
+(defn view-model
+  []
+  (reify
+    venue/IHandleEvent
+    (handle-event [owner event args cursor]
+      (event-handler event owner args cursor))
+    venue/IHandleResponse
+    (handle-response [owner outcome event data]
+      (response-handler [event outcome] owner data))
+    venue/IActivate
+    (activate [owner args cursor])))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defmethod event-handler
   :login
-  [_ this {:keys [email password] :as login-args} cursor]
+  [_ owner {:keys [email password] :as login-args} cursor]
   (log/debug "Logging in..." email password)
-  (venue/request! this :service/data :login login-args))
+  (venue/request! owner :service/data :login login-args))
 
 (defmethod event-handler
   :test-event
-  [_ this new-text cursor]
+  [_ owner new-text cursor]
   (om/update! cursor :text new-text))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defmethod response-handler
   [:login :success]
-  [_ data]
+  [_ owner data]
   (log/debug "RESPONSE HANDLER SUCCESS"))
 
 (defmethod response-handler
   [:login :failure]
-  [_ data]
+  [_ owner data]
   (log/debug "RESPONSE HANDLER FAILURE"))
-
-(defn view-model
-  []
-  (reify
-    venue/IHandleEvent
-    (handle-event [this event args cursor]
-      (event-handler event this args cursor))
-    venue/IHandleResponse
-    (handle-response [this outcome event data]
-      (response-handler [event outcome] data))
-    venue/IActivate
-    (activate [_ args cursor])))

46a3c0303ea9a083388a43a3029444382f40db03
Antony Woods <antony@mastodonc.com>
2015-09-28 12:57:18 +0100

    Added clojars to deploy

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mastodonc/venue"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
+  :lein-release {:deploy-via :clojars}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-3297"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]

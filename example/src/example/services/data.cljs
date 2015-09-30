fab53a204f288f5902b4f725e848bd28db0941ac
Antony Woods <antony@mastodonc.com>
2015-09-30 17:34:36 +0100

    Add ability to "init" services
    Services that implement "IInitialise" will now be initialised when venue
    starts. Also added a test to check this.

--- example/src/example/services/data.cljs
+++ example/src/example/services/data.cljs
@@ -1,8 +1,22 @@
-(ns example.services.data)
+(ns example.services.data
+  (:require [venue.core :as venue])
+  (:require-macros [cljs-log.core :as log]))
 
-(defmulti handler (fn [event args] event))
+(defmulti response-handler
+  (fn [result response cursor] result))
 
-(defmethod handler
-  :login
-  [_ args]
-  [:success {:token "foobar"}])
+(defmulti request-handler
+  (fn [event args response-ch] event))
+
+(defn service
+  []
+  (reify
+    venue/IHandleRequest
+    (handle-request [owner request args response-ch]
+      (request-handler request args response-ch))
+    venue/IHandleResponse
+    (handle-response [owner outcome event response cursor]
+      (response-handler [event outcome] response cursor))
+    venue/IInitialise
+    (initialise [owner cursor]
+      (log/debug "DATA SERVICE INIT"))))

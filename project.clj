1c4eab2671b057a26114128ee7bc35d82d68424c
Antony Woods <antony@mastodonc.com>
2015-10-01 09:50:55 +0100

    lein-release plugin: preparing 0.1.5 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject venue "0.1.5-SNAPSHOT"
+(defproject venue "0.1.5"
   :description "Experimental MVVM-like framework for ClojureScript"
   :url "https://github.com/mastodonc/venue"
   :license {:name "Eclipse Public License"

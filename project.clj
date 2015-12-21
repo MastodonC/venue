b86fef8db3bc85c3eae1197210cc149391e9a50e
Antony Woods <antony@mastodonc.com>
2015-12-21 11:14:41 +0000

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject venue "0.1.8"
+(defproject venue "0.1.9-SNAPSHOT"
   :description "Experimental MVVM-like framework for ClojureScript"
   :url "https://github.com/mastodonc/venue"
   :license {:name "Eclipse Public License"
